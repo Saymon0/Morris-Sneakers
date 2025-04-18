98019131b1b63a8b2125dd5dc4613f3d83f05e39
Kostya <kostyanaumencko@yandex.ru>
2025-04-18 23:35:18 +0700

    Добавил colormode в vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), ui(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), ui({ colormode: false }), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
